8a36f25b86e531aa05c08911b8433f253450122b
KunalTakke <kunal.technophilic@gmail.com>
2025-02-03 23:16:37 +0530

    Added program for ReverseNumber

---
+++ 03-programming-fundamentals-1/programming-fundamentals-1/JavaBasics/src/com/basic/java/ReverseNumber.java
@@ -0,0 +1,24 @@
+package com.basic.java;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+
+	public static void main(String[] args) {
+		
+		Scanner sc= new Scanner(System.in);
+		System.out.print("Enter the number :");
+		int n = sc.nextInt();
+		// form the reverse number
+		int result=0;
+		while(n!=0) {
+			int rem = n%10;
+			result=result*10+rem;
+			n=n/10;
+		}
+		
+		System.out.println(result);
+		
+	}
+
+}
