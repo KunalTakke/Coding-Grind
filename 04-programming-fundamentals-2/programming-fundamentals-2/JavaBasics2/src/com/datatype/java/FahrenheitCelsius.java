7fdd852dc1771ae9129273f71ca7afbee51146ad
KunalTakke <kunal.technophilic@gmail.com>
2025-02-11 06:42:11 +0530

    Added Fahrenheit to Celsius Program

---
+++ 04-programming-fundamentals-2/programming-fundamentals-2/JavaBasics2/src/com/datatype/java/FahrenheitCelsius.java
@@ -0,0 +1,17 @@
+package com.datatype.java;
+
+public class FahrenheitCelsius {
+
+	public static void main(String[] args) {
+
+		System.out.println("Fahr" + " " + "Celsius");
+		int f = 0;
+		while (f <= 300) {
+			int celsius = (int) ((5.0 / 9) * (f - 32)); //type cast double to integer
+			System.out.println(f + "\t" + celsius);
+			f += 20; // increment by 20
+		}
+
+	}
+
+}
