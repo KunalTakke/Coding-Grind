67d3c1af593d027c31d019c204f1a9f9b4dd2280
KunalTakke <kunal.technophilic@gmail.com>
2025-03-03 17:44:17 +0530

    Added Program for PatternNumberStars2

---
+++ Challenges-Fundamentals/Challenge2/PatternChallenge/src/com/challenge/java/PatternNumberStars2.java
@@ -0,0 +1,55 @@
+package com.challenge.java;
+
+import java.util.Scanner;
+/*
+ n = 7
+ 
+ 1******
+ 12*****
+ 123****
+ 1234***
+ 12345**
+ 123456*
+ 1234567
+  
+ */
+
+public class PatternNumberStars2 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		int row = 1;
+		int nwork = row;
+		int snwork = n - nwork; 
+		while(row<=n) {
+			
+			// number work
+			int cwork = 1;
+			while(cwork<=nwork) {
+				System.out.print(cwork);
+				cwork++;
+			}
+			
+			// star work
+			int scwork = 1;
+			while(scwork<=snwork) {
+				System.out.print("*");
+				scwork++;
+			}
+			
+			
+			System.out.println();
+			
+			nwork++;
+			snwork = n - nwork;
+			row ++;
+		}
+		
+		
+
+	}
+
+}
