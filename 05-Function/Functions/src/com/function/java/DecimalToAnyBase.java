2db87d5c682a4657b3a75f131b4ef58466c8cef7
KunalTakke <kunal.technophilic@gmail.com>
2025-03-03 17:26:24 +0530

    Added Program for decimalToAnyBase

--- 05-Function/Functions/src/com/function/java/DecimalToAnyBase.java
+++ 05-Function/Functions/src/com/function/java/DecimalToAnyBase.java
@@ -3,10 +3,36 @@ package com.function.java;
 import java.util.Scanner;
 
 public class DecimalToAnyBase {
+	
+	public static int decimalToAnyBase(int number, int destBase) {
+		
+		int multiplier = 1;
+		int result = 0;
+		int sbase = 10;
+		while(number!=0) {
+			
+			int lastDigit = number % destBase;
+			result = result + lastDigit * multiplier;
+			multiplier = multiplier * sbase ; 
+			
+			
+			number = number/destBase;
+		}
+		
+		
+		
+		return result;
+	}
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int dbase = sc.nextInt();
 		
+		
+		int res = decimalToAnyBase(n, dbase);
+		System.out.println("The result in base "+dbase+" is :"+res);
 
 	}
 
