085356f13c11c3b3d08fb0224ee77ead594b7f54
KunalTakke <kunal.technophilic@gmail.com>
2025-04-05 21:30:24 +0530

    Added Program for substring

---
+++ 07-MoreAboutArrays/MoreAboutArrays/src/com/adv/arrays/StrSubstring.java
@@ -0,0 +1,17 @@
+package com.adv.arrays;
+
+public class StrSubstring {
+
+	public static void main(String[] args) {
+		
+		String str = "Hello";
+		System.out.println(str.substring(0,1));
+		System.out.println(str.substring(0));
+		System.out.println(str.substring(2));
+		System.out.println(str.substring(3,3));
+		System.out.println(str.substring(1,5));
+		
+
+	}
+
+}
