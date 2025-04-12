741b2697ff0bebf2ba0d5a69bf73135236864f32
KunalTakke <kunal.technophilic@gmail.com>
2025-04-12 17:45:13 +0530

    Added Program for TargetSumPairs

---
+++ Challenges-Arrays/arrays-challenges/src/com/challenge/arrays/TargetSumPairs.java
@@ -0,0 +1,35 @@
+package com.challenge.arrays;
+
+import java.util.Scanner;
+
+public class TargetSumPairs {
+
+	public static void main(String[] args) {
+
+		Scanner scn = new Scanner(System.in);
+		int arrSize = scn.nextInt();
+
+		int[] arr = new int[arrSize];
+
+		for (int i = 0; i < arrSize; i++) {
+			arr[i] = scn.nextInt();
+		}
+
+		int querySum = scn.nextInt();
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] + arr[j] == querySum) {
+					if (arr[i] <= arr[j]) {
+						System.out.println(arr[i] + " and " + arr[j]);
+					} else {
+						System.out.println(arr[j] + " and " + arr[i]);
+					}
+
+				}
+			}
+		}
+
+	}
+
+}
